e912fc964006e265e8eac927e11d638f7cda778e
Ivan Rojo <ivanrojo07@gmail.com>
2020-09-20 20:30:32 -0500

    119.- OnDestroy

--- app/src/main/java/com/example/oncreate/MainActivity.kt
+++ app/src/main/java/com/example/oncreate/MainActivity.kt
@@ -49,4 +49,9 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
         Toast.makeText(this,"Aplicativo visible", Toast.LENGTH_SHORT).show()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this,"App destruida", Toast.LENGTH_SHORT).show()
+    }
 }
